8c054ee54580a24c13d12ad0604ab06484979725
sebas <personaldesebas@gmail.com>
2024-10-15 16:25:45 +0200

    subida ejercicio promoción

---
+++ promocion/js/index.js
@@ -0,0 +1,4 @@
+changeToAsignaturas = () =>{
+    //https://www.iestetuan.es/cmsj/9-ensenanzas/informatica-y-comunicaciones/8-daw-desarrollo-de-aplicaciones-web.html/#AutoNumber1
+    document.getElementById("frameMenu").src = "https://www.iestetuan.es/cmsj/9-ensenanzas/informatica-y-comunicaciones/8-daw-desarrollo-de-aplicaciones-web.html/#AutoNumber1";
+}
