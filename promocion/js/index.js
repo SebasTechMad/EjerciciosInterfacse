66cf16b6ef13a6d1b7e34286c361a7b7e8c8c280
sebas <personaldesebas@gmail.com>
2024-10-17 00:10:22 +0200

    subida actualizacion página

--- promocion/js/index.js
+++ promocion/js/index.js
@@ -2,3 +2,11 @@ changeToAsignaturas = () =>{
     //https://www.iestetuan.es/cmsj/9-ensenanzas/informatica-y-comunicaciones/8-daw-desarrollo-de-aplicaciones-web.html/#AutoNumber1
     document.getElementById("frameMenu").src = "https://www.iestetuan.es/cmsj/9-ensenanzas/informatica-y-comunicaciones/8-daw-desarrollo-de-aplicaciones-web.html/#AutoNumber1";
 }
+
+changeToProfesorado = () =>{
+    document.getElementById("frameMenu").src = "https://www.iestetuan.es/varios/2024horarios/DAW2A_12.html";
+}
+
+changeToSecretaria = () =>{
+    document.getElementById("frameMenu").src = "https://www.iestetuan.es/cmsj/secretaria/información-general.html/#main";
+}
