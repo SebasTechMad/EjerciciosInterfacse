84ac5c2ac953629de76a2368c8271c43d35fbe48
sebas <personaldesebas@gmail.com>
2024-11-15 12:48:56 +0100

    subida nuevos archivos articulos + responsive tablet

--- proyecto-blog/js/index.js
+++ proyecto-blog/js/index.js
@@ -2,14 +2,30 @@ const nav = document.querySelector(".nav");
 const abrir = document.querySelector("#btnAbrirMenu");
 const cerrar = document.querySelector("#btnCerrarMenu");
 
-console.log(abrir);
+const articulos = document.getElementsByClassName("info");
 
+console.log(articulos);
+
+//EVENTOS PARA MENÚ HAMBURGUESA
 abrir.addEventListener("click", () =>{
     nav.classList.add("visible");
     abrir.classList.add("invisible");
-})
+});
 
 cerrar.addEventListener("click", () =>{
     nav.classList.remove("visible");
     abrir.classList.remove("invisible");
-})
+});
+
+
+for (const articulo of articulos) {
+     articulo.addEventListener("click", () =>{
+        //location.href = `./articulos/${articulo.id}.html`;
+        location.href = "articulos/"+articulo.id+".html";
+     });
+  }
+
+
+//EVENTO ARTICULOS
+
+
