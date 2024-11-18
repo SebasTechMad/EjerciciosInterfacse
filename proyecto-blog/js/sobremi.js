ebcbfb79119ac7254b4e065381c741e7132409bb
sebas <personaldesebas@gmail.com>
2024-11-19 00:25:34 +0100

    actualizacion

--- proyecto-blog/js/sobremi.js
+++ proyecto-blog/js/sobremi.js
@@ -3,7 +3,7 @@ const reflexionPDF = document.getElementById("reflexion");
 const guiaEstiloPDF = document.getElementById("guiaEstilo");
 
 prototipoPDF.addEventListener("click",() =>{
-    location.href ="./home.html";
+    window.open("../srcs/pdf/blog-prototipo.pdf", 'blank');
 });
 
 reflexionPDF.addEventListener("click",() =>{
@@ -11,5 +11,5 @@ reflexionPDF.addEventListener("click",() =>{
 });
 
 guiaEstiloPDF.addEventListener("click",() =>{
-    location.href ="./home.html";
+    window.open("../srcs/pdf/guia-estilos.pdf",'blank');
 });
