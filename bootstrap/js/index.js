c7c5215efeab0767b54c2ce44d1415060871106f
SebasTechMad <personaldesebas@gmail.com>
2024-12-14 15:40:47 +0100

    subida actualizacion practica bootstrap

---
+++ bootstrap/js/index.js
@@ -0,0 +1,17 @@
+listadoLinks = ["./funciones/indexFormulario.html","./funciones/indexListGroups.html","./funciones/indexNavs.html"];
+console.log(listadoLinks);
+
+opciones = document.getElementsByTagName("article");
+
+let redirigir1 = () =>{
+
+    location.href = "./funciones/indexFormulario.html";
+
+}
+
+
+
+
+// opciones[0].addEventListener('click',() => {
+//     location.href = "./funciones/indexFormulario.html";
+// });
