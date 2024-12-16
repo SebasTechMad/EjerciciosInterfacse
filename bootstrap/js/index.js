2e8c06c414d0d554dfc21408d6983f8be4731f7e
SebasTechMad <personaldesebas@gmail.com>
2024-12-16 02:30:06 +0100

    subida finalizacion ejercicio bootstrap

--- bootstrap/js/index.js
+++ bootstrap/js/index.js
@@ -4,9 +4,15 @@ console.log(listadoLinks);
 opciones = document.getElementsByTagName("article");
 
 let redirigir1 = () =>{
+    location.href = "./funciones/indexNavs.html";
+}
 
+let redirigir2 = () =>{
+    location.href = "./funciones/indexListGroups.html";
+}
+
+let redirigir3 = () =>{
     location.href = "./funciones/indexFormulario.html";
-
 }
 
 
