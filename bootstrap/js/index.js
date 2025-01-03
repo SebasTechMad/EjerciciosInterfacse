83d4465f79e258ea50c88183d8f1d0d2b434aebf
SebasTechMad <personaldesebas@gmail.com>
2025-01-03 02:53:43 +0100

    actualizacion proyecto, add slider + optimizacion svg imagenes

--- bootstrap/js/index.js
+++ bootstrap/js/index.js
@@ -1,21 +1,8 @@
+
+/**OJO, HE OPTIMIZADO EL CÓDIGO PARA QUE SOLO SE TENGA QUE AÑADIR LA URL DE CADA PÁGINA. PERO HAY 
+ * QUE HACERLO DE MANERA ORDENADA PARA QUE ESTE PUEDA COINCIDIR LA URL CON LA PÁGINA CORRESPONDIENTE*/
 opcionesMenu = ["./funciones/indexNavs.html","./funciones/indexListGroups.html","./funciones/indexFormulario.html"];
-
 opciones = document.getElementsByTagName("article");
-
-
-/*let redirigir1 = () =>{
-    location.href = "./funciones/indexNavs.html";
-}
-
-let redirigir2 = () =>{
-    location.href = "./funciones/indexListGroups.html";
-}
-
-let redirigir3 = () =>{
-    location.href = "./funciones/indexFormulario.html";
-}*/
-
-
 
 let loadPages = () => {
     var cont = 0;
