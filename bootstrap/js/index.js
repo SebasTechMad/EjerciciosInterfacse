b97797115009e89d72c05200ac3cac4d18461870
SebasTechMad <personaldesebas@gmail.com>
2025-01-22 03:46:57 +0100

    fdsjhklsjsfñlk

--- bootstrap/js/index.js
+++ bootstrap/js/index.js
@@ -2,7 +2,12 @@
 /**OJO, HE OPTIMIZADO EL CÓDIGO PARA QUE SOLO SE TENGA QUE AÑADIR LA URL DE CADA PÁGINA. PERO HAY 
  * QUE HACERLO DE MANERA ORDENADA PARA QUE ESTE PUEDA COINCIDIR LA URL CON LA PÁGINA CORRESPONDIENTE*/
 opcionesMenu = ["indexDropdowns.html", "indexNavs.html","indexScrollSpy.html","indexListGroups.html",
-    "indexFormulario.html"];
+    "indexForms.html", "indexInputGroups.html", "indexSelectMenus.html", "indexRange.html",
+    "indexChecks.html", "indexFormValidation.html", "indexFloatingLabel.html", "indexButton.html",
+    "indexTables.html", "indexBadges.html", "indexButtonGroups.html", "indexSpinners.html", "indexCards.html","indexDarkMode.html",
+    "indexImages.html", "indexModal.html", "indexToolTip.html", "indexPopOver.html", "indexToast.html",
+    "indexCollapse.html", "indexOffCanvas.html", "indexCarousel.html", "indexProgressBar.html", "indexAlerts.html",
+    "indexPagination.html", "indexFlex.html"];
 opciones = document.getElementsByTagName("article");
 
 let loadPages = () => {
