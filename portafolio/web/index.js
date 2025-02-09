26e2960d1adc5327a0cfc8d3faf0201c2621f152
SebasTechMad <personaldesebas@gmail.com>
2025-02-09 14:48:59 +0100

    subida actualizacion

--- portafolio/web/index.js
+++ portafolio/web/index.js
@@ -37,10 +37,23 @@ let cargarVariables = () =>{
 }
 
 var addConversacionQuienEres = () =>{
+
+    let chat = document.getElementById("mensajes");
+
+
     user_respuesta = document.getElementById("msg_user");
     bot_respuesta = document.getElementById("msg_bot");
 
-    user_respuesta.innerHTML += "<h1>quien sos</h1>";
+    
+    let respuestaU = '<div class="d-flex justify-content-end"><div class="d-flex flex-column align-items-end"><div class="d-flex justify-content-end gap-4"><span><b>Usuario</b></span><span></span></div><div class="d-flex justify-content-end gap-2"><span class="badge text-bg-primary text-light texto-chat text-end" name="mensaje" style="width: fit-content;">¿Quien eres?</span><i class="bi bi-person-circle text-light"></i></div></div></div>';
+    let respuestaB = '<div class="align-self-start" id="msg_bot"><div class="d-flex justify-content-start"><div class="d-flex flex-column align-items-start"><div class="d-flex justify-content-start gap-4"><span></span><span><b>Bot</b></span></div><div class="d-flex justify-content-start gap-2"><i class="bi bi-person-circle text-light"></i><span class="badge text-bg-success text-light texto-chat" style="width: fit-content;" name="mensaje"><div class="card mb-3" style="max-width: 400px;"><div class="card-header"><b>Juan Sebastián Osorio</b></div><div class="row g-0"><div class="col-md-4"><img src="./web/srcs/sebasCV.jpg" class="img-fluid rounded-start p-2" alt="Imagen de prueba"></div><div class="col-md-8"><div class="card-body d-flex flex-column align-items-center"><p class="card-text text-center">Desarrollador <strong>full-stack</strong>, enfocado en ser <strong>SecDevOps</strong>, aplicando seguridad en las aplicaciones web, además de haber sido candidato en el <strong>campus de Madrid 42</strong>.</p></div></div></div><div class="card-footer"><div class="d-flex gap-2"><button type="button" class="btn btn-outline-success" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="E-mail copiado"><i class="bi bi-clipboard2-fill"></i></button><a href="./web/srcs/SebastíanOsorio.pdf" target="_blank" class="btn btn-outline-danger"><i class="bi bi-file-earmark-pdf-fill"></i></a><a href="https://www.linkedin.com/in/sebastián-osorio-518728266/" target="_blank" class="btn btn-outline-primary"><i class="bi bi-linkedin"></i></a><a href="https://github.com/SebasTechMad" target="_blank" class="btn btn-outline-dark"><i class="bi bi-github"></i></a></div></div></div></span></div></div></div></div>';
+    
+    
+    
+    
+    chat.innerHTML += respuestaU;
+    chat.innerHTML += respuestaB;
+    chat.innerHTML += "<br><br>";
 }
 
 
